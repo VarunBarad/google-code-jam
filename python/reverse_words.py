b6ef6d8a67a6a58b7701ba31f68b6763de691ed0
Varun Barad <varunbarad@live.com>
2015-01-23 23:09:32 +0530

    Reverse Words finished

---
+++ python/reverse_words.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+__author__ = 'Varun Barad'
+
+
+def main():
+    no_of_test_cases = int(input())
+    for i in range(no_of_test_cases):
+        words = input().split(' ')
+        print('Case #{0}: {1}'.format((i+1), ' '.join(words[::-1])))
+
+if __name__ == '__main__':
+    main()
